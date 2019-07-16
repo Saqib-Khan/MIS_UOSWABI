c74b9696068871c7b61967b4fcb761ff8fb34908
Saqib Khan <skhan@globalrescue.com>
2019-07-16 15:58:05 +0500

    Final Site Layout, can add more Webforms/Pages here.

--- MIS_UOSWABI/App_Start/BundleConfig.cs
+++ MIS_UOSWABI/App_Start/BundleConfig.cs
@@ -32,7 +32,15 @@ namespace MIS_UOSWABI
             // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                            "~/Scripts/modernizr-*"));
+                            "~/Scripts/jquery-*",
+                            "~/Scripts/modernizr-*",
+                            "~/Scripts/site.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                            "~/Content/bootstrap-grid.css",
+                            "~/Content/bootstrap.css",
+                             "~/Content/site.css",
+                            "~/Content/navbar.css"));
         }
     }
 }
---
+++ MIS_UOSWABI/Scripts/Site.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(".nav-link").on("click", (event) => {
+        $(".navbar-nav").find(".active").removeClass('active');
+        $(event.target).parent().addClass('active');
+    });
+
+    var url = window.location;
+    $('.navbar .nav').find('.active').removeClass('active');
+    $('.navbar .nav li a').each(function () {
+        if (this.href == url) {
+            $(this).parent().addClass('active');
+        }
+    }); 
+});
